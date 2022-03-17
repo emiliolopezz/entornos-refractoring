e3f24326ce4b0f40d41650dd0f6b1e580202aca2
leon99crak <emiliolopezln12@gmail.com>
2022-03-17 16:32:52 +0100

    refractoring 2

--- Tarea_4/proyecto/src/main/java/Rental.java
+++ Tarea_4/proyecto/src/main/java/Rental.java
@@ -1,6 +1,11 @@
+
+import java.util.Enumeration;
+import java.util.Vector;
+
 public class Rental {
     private Movie _movie;
     private int _daysRented;
+    private Vector _rentals = new Vector();
 
     public Rental (Movie movie, int daysRented) {
         _movie = movie;
@@ -13,5 +18,28 @@ public class Rental {
 
     public Movie getMovie () {
         return _movie;
+    }
+    private double  getCharge()    {
+        double thisAmount = 0;
+        Enumeration rentals = _rentals.elements();
+        Rental each = (Rental) rentals.nextElement();
+            
+          // determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+            return thisAmount;
     }
 }
